d19809d187fb05f9df36af9fc7d51b1788a28423
Misha Plehanov <mih.plekhanov@yandex.ru>
2026-05-24 00:43:40 +0300

    Убрать двоичные файлы для GH

--- src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.item.mapper;
 import org.springframework.stereotype.Component;
 import ru.practicum.shareit.item.dto.ItemResponseDto;
 import ru.practicum.shareit.item.dto.NewItemRequestDto;
-import ru.practicum.shareit.item.dto.UpdateItemRequestDto;
 import ru.practicum.shareit.item.model.Item;
 
 @Component
